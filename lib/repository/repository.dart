1bf0c911cb3ef55016620af3100208ca9dd62687
Naman2004singh <naman.2singh9@gmail.com>
2025-05-02 21:52:33 +0530

    added the user respository for the auth

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,9 +1,23 @@
 import 'package:vedavita/data/network/network_api_service.dart';
+import 'package:vedavita/models/auth/login_model.dart';
+import 'package:vedavita/models/auth/register_model.dart';
 import 'package:vedavita/models/chat_response_model.dart';
 import 'package:vedavita/utils/app_urls.dart';
 
 class Repository {
   final _api = NetworkApiService();
+
+   // register api
+  Future<RegisterResponse> registerApi(dynamic data) async {
+    final response = await _api.postApi(AppUrls.registerUrl, data);
+    return RegisterResponse.fromJson(response);
+  }
+
+  // login api
+  Future<LoginResponse> loginApi(dynamic data) async {
+    final response = await _api.postApi(AppUrls.loginUrl, data);
+    return LoginResponse.fromJson(response);
+  }
 
   Future<ChatResponseModel> sendMessage(String message) async {
     final response = await _api.postApi(
