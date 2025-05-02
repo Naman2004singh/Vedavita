984bd2b25fb5d51540f1f7159fbdacddf31aa853
Naman2004singh <naman.2singh9@gmail.com>
2025-05-02 15:42:15 +0530

    added the app exceptions

---
+++ lib/data/exception/app_exceptions.dart
@@ -0,0 +1,28 @@
+class AppExceptions implements Exception {
+  final _message;
+  final _prefix;
+  AppExceptions([this._message, this._prefix]);
+
+  @override
+  String toString() {
+    return '$_message  $_prefix';
+  }
+}
+class NoInternetException extends AppExceptions {
+  NoInternetException([String? message])
+      : super(message, 'No Internet connection');
+}
+
+class UnAuthorizedException extends AppExceptions {
+  UnAuthorizedException([String? message]) : super(message, "You don't have access to this");
+}
+
+class RequestTimeOutException extends AppExceptions {
+  RequestTimeOutException([String? message])
+      : super(message, 'Request time out');
+}
+
+class FetchDataException extends AppExceptions {
+  FetchDataException([String? message])
+      : super(message, 'Fetching error');
+}
